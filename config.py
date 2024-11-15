e4508fe3baaa405a3e4e04519341d750b21f267e
Sopedia <soran.hassan21@gmail.com>
2024-11-15 12:48:44 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
-HASH_ID = 'ca09cdd51d752e9d7001b37faa2e0b1e'
-API_ID = 29990061
-BOT_TOKEN = ''
-PHONE_NUMBER = 0
-PROXY = {
-    "proxy_type": "socks5",
-    "addr":"tate2.veestore.shop",
-    "port": 37964,
-    "rdns": True,
-    "username": "VeePassTxA",
-    "password": "b8lnM9Oqrg"
-}
+HASH_ID = 'YOUR HASH ID'
+API_ID = 'YOUR API ID'
+BOT_TOKEN = 'BOT_TOKEN'
+PHONE_NUMBER = 'YOUR PHONE NUMBER'
